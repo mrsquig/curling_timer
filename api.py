2042f71b911018bea35259fb035f810163b73fb4
mrsquig <zac@zaclevinson.com>
2024-11-06 09:44:00 -0500

    Enhance API to have a countdown mode, calculate hours, and to have an override to set the starting timestamp to a specific time.

--- api.py
+++ api.py
@@ -19,7 +19,8 @@ app_config = {
     "version": ConfigValue(0.1),
     "time_per_end": ConfigValue(15*60, int),
     "num_ends": ConfigValue(8, int),
-    "start_timestamp": ConfigValue(timestamp(), int)
+    "start_timestamp": ConfigValue(timestamp(), int),
+    "count_direction": ConfigValue(-1, int),
 }
 
 @app.route('/version', methods=['GET'])
@@ -53,20 +54,42 @@ def update_config():
 @app.route('/reset', methods=['GET'])
 def reset_timer():
   global app_config
-  app_config["start_timestamp"].value = timestamp()
+  time = request.args.get('time')
+  if not time:
+    app_config["start_timestamp"].value = timestamp()
+  else:
+    app_config["start_timestamp"].value = time
+
   return jsonify({"start_timestamp": app_config["start_timestamp"].value}), 200
 
 @app.route('/game_times', methods=['GET'])
 def get_times():
+  # Get how long the timer has been running
   current_time = timestamp()
   uptime = int(current_time) - app_config["start_timestamp"].value
 
+  # Initialize results dictionary
   times = {}
-  times["minutes"] = uptime // 60
-  times["seconds"] = uptime % 60
+
+  # End number and percentage are always based on the uptime of the timer
   times["end_number"] = uptime // app_config["time_per_end"].value + 1
   times["end_percentage"] = uptime/app_config["time_per_end"].value - times["end_number"] + 1
 
+  # Calculate the total time of the game, then figure out which time to split into
+  # hours, minutes, and seconds depending on if we're counting down or up
+  times["total_time"] = app_config["time_per_end"].value * app_config["num_ends"].value
+  game_time = times["total_time"] - uptime if app_config["count_direction"].value < 0 else uptime
+
+  # If we're over time, then return how far over time we are
+  times["overtime"] = uptime > times["total_time"]
+  if times["overtime"] and app_config["count_direction"].value < 0:
+    game_time = game_time*-1
+
+  # Divide the time into hours, minutes, seconds
+  times["hours"] = game_time // 3600
+  times["minutes"] = (game_time // 60) % 60
+  times["seconds"] = game_time % 60
+    
   return jsonify(times), 200
 
 if __name__ == '__main__':
